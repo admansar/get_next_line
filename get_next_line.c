9678e17ec14ebcca49c4eb1ee3a457a8c471010b
admansar <115159995+admansar@users.noreply.github.com>
2022-11-30 20:34:34 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: admansar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 18:57:18 by admansar          #+#    #+#             */
-/*   Updated: 2022/11/29 14:07:30 by admansar         ###   ########.fr       */
+/*   Updated: 2022/11/29 18:23:03 by admansar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,16 @@ char	*get_save(char *save)
 		i++;
 	if (save[i] == '\n')
 		i++;
-	if(save[i] == '\0')
+	if (save[i] == '\0')
 	{
 		free(save);
-		return(NULL);
+		return (NULL);
 	}
 	s = malloc(sizeof(char) * ft_strlen(save) - i + 1);
 	if (!s)
 		return (NULL);
 	while (save[i])
-	{
 		s[j++] = save[i++];
-	}
 	s[j] = '\0';
 	free(save);
 	return (s);
@@ -72,31 +70,27 @@ char	*get_the_line(char *save)
 char	*get_read(int fd, char *save)
 {
 	int		c;
-	int		looper;
 	char	*ptr;
 
 	c = 1;
-	looper = 1;
 	ptr = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!ptr)
 		return (NULL);
-	while (c > 0 && looper == 1)
+	while (c > 0)
 	{
 		c = read(fd, ptr, BUFFER_SIZE);
-		if(c == 0)
-			break;
-		if(c < 0 && save)
+		if (c < 0 && save)
 		{
 			free(save);
 			save = NULL;
-			break;
+			break ;
 		}
-		else if(c < 0)
-			break;
+		else if (c <= 0)
+			break ;
 		ptr[c] = '\0';
 		save = ft_strjoin(save, ptr);
 		if (ft_strchr(save, '\n'))
-			looper = 0;
+			break ;
 	}
 	free(ptr);
 	return (save);
